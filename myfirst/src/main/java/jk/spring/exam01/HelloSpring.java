f60da6ddb57f48248a1b1fa10807b4caf5821783
worhk <worhk@DESKTOP-CHV865T>
2022-05-31 02:52:55 +0900

    First Commit

---
+++ myfirst/src/main/java/jk/spring/exam01/HelloSpring.java
@@ -0,0 +1,11 @@
+package jk.spring.exam01;
+
+public class HelloSpring {
+
+	public static void main(String[] args) {
+		Mybean bean = new Mybean();
+		bean.sayHello("Spring");
+		
+	}
+
+}
---
+++ myfirst/src/main/java/jk/spring/exam01/Mybean.java
@@ -0,0 +1,9 @@
+package jk.spring.exam01;
+
+public class Mybean {
+	public void sayHello(String name) {
+		
+		System.out.println("Hello , " + name);
+		
+	}
+}
