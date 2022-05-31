40e7328e3402d4902dfe93a7b4179e2618eed710
Jaekwan Kim <worhks5017@naver.com>
2022-05-31 12:37:35 +0900

    5/31 Practice

--- myfirst/src/main/java/jk/spring/exam01/HelloSpring.java
+++ myfirst/src/main/java/jk/spring/exam01/HelloSpring.java
@@ -4,7 +4,7 @@ public class HelloSpring {
 
 	public static void main(String[] args) {
 		Mybean bean = new Mybean();
-		bean.sayHello("½ºÇÁ¸µ!");
+		bean.sayHello("Spring!");
 		
 	}
 
--- myfirst/src/main/java/jk/spring/exam01/Mybean.java
+++ myfirst/src/main/java/jk/spring/exam01/Mybean.java
@@ -3,7 +3,7 @@ package jk.spring.exam01;
 public class Mybean {
 	public void sayHello(String name) {
 		
-		System.out.println("æ»≥Á«œººø‰ ," + name);
+		System.out.println("Hello ," + name);
 		
 	}
 }
